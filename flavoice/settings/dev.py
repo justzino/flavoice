5a4e9712596068850aedd9a3dcb8b19eb8c172bf
Jinho Lee <49632119+justzino@users.noreply.github.com>
2021-06-10 18:41:45 +0900

    BUILD: 프로젝트 초기 셋팅

---
+++ flavoice/settings/dev.py
@@ -0,0 +1,25 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# False if not in os.environ
+DEBUG = env.bool("DEBUG", True)
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, "envs/.env.dev"))
+
+# https://docs.djangoproject.com/en/3.1/ref/settings/#allowed-hosts
+ALLOWED_HOSTS += env.list("DJANGO_ALLOWED_HOSTS")
+THIRD_PARTY_APPS += [
+]
+
+# Database ------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": env("SQL_ENGINE"),
+        "NAME": env("SQL_DATABASE"),
+        "USER": env("SQL_USER"),
+        "PASSWORD": env("SQL_PASSWORD"),
+        "HOST": env("SQL_HOST"),
+        "PORT": env("SQL_PORT"),
+    }
+}
