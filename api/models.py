ea257f95b7c8a2f0f92da4807829021ef31f8b2e
Jinho Lee <49632119+justzino@users.noreply.github.com>
2021-09-14 19:50:31 +0900

    REFACTOR: Add a file model

--- api/models.py
+++ api/models.py
@@ -30,28 +30,31 @@ class TimeStampedModel(models.Model):
 
 class Voice(TimeStampedModel):
 
-    """ Voice Model """
+    """ Voice Model to analyze and save pitches """
 
-    # 음역대
-    PITCH_CHOICES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    # 목소리 음역대 저장
+    max_pitch = models.CharField(max_length=2, blank=True, null=True)
+    min_pitch = models.CharField(max_length=2, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='voices')
 
-    max_pitch = models.CharField(choices=PITCH_CHOICES, max_length=2, blank=False, null=False)
-    min_pitch = models.CharField(choices=PITCH_CHOICES, max_length=2, blank=False, null=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='songs')
+
+class File(TimeStampedModel):
+
+    """ File Model to save voice files """
+
+    filename = models.FileField(upload_to="voices", blank=False, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='files')
 
 
 class Song(TimeStampedModel):
 
-    """ Song Model """
-
-    # 음역대
-    PITCH_CHOICES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    """ Song Model to save songs with pitch and info """
 
     title = models.CharField(max_length=200, blank=False, null=False)
-    max_pitch = models.CharField(choices=PITCH_CHOICES, max_length=2, blank=False, null=False)
-    min_pitch = models.CharField(choices=PITCH_CHOICES, max_length=2, blank=False, null=False)
-    singer = models.ForeignKey('Singer', on_delete=models.SET_NULL, null=True)
-    genre = models.ManyToManyField('Genre', help_text='Select a genre for this song')
+    max_pitch = models.CharField(max_length=2, blank=False, null=False)  # 음역대
+    min_pitch = models.CharField(max_length=2, blank=False, null=False)
+    singer = models.ForeignKey('Singer', on_delete=models.SET_NULL, null=True, related_name='songs')
+    genre = models.ManyToManyField('Genre', related_name='songs', help_text='Select a genre for this song')
 
     def __str__(self):
         """String for representing the Model object."""
@@ -60,7 +63,7 @@ class Song(TimeStampedModel):
 
 class Genre(TimeStampedModel):
 
-    """ Model representing a song genre. """
+    """ Genre Model """
 
     name = models.CharField(max_length=200, help_text='Enter a song genre (e.g. Hip-Hop)')
 
@@ -71,7 +74,7 @@ class Genre(TimeStampedModel):
 
 class Singer(models.Model):
 
-    """ Model representing a Singer. """
+    """ Singer Model """
 
     kor_name = models.CharField(max_length=100)
     eng_name = models.CharField(max_length=100, null=True, blank=True)
